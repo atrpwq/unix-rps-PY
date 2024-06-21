746dc2991ed232a6c872d4fd80888a8a86c0dd38
atr <atrpwq@proton.me>
2024-06-21 23:17:54 +0300

    Update rps.py

--- rps.py
+++ rps.py
@@ -6,40 +6,31 @@ paperlst = ['P','p','PAPER','paper']
 scissorslst = ['S','s','SCISSORS','scissors']
 ranlst = ['r','p','s']
 score = 0
+def win():
+    print("win")
+    score+=1
+def tie():print("tie")
+def loss():
+    print("loss")
+    score-=1
 while True:
     os.system('clear')
     ranchoice = random.choice(ranlst)
-    if score <= 0:
-        score = 0
+    if score <= 0:score = 0
     print(f'score: {score}')
     choice = input('rock, paper, scissors [r/p/s] >> ')
     if choice == 'r':
-        if ranchoice == 'r':
-            print("tie")
-        elif ranchoice == 'p':
-            print("loss")
-            score -= 1
-        else:
-            print("win")
-            score += 1
+        if ranchoice == 'r':tie()
+        elif ranchoice == 'p':loss()
+        else:win()
     elif choice == 'p':
-        if ranchoice == 'r':
-            print("win")
-            score += 1
-        elif ranchoice =='p':
-            print("tie")
-        else:
-            print("loss")
-            score -= 1
+        if ranchoice == 'r':win()
+        elif ranchoice =='p':tie()
+        else:loss()
     elif choice == 's':
-        if ranchoice == 'r':
-            print('loss')
-            score -= 1
-        elif ranchoice == 'p':
-            print("win")
-            score += 1
-        else:
-            print('tie')
+        if ranchoice == 'r':loss()
+        elif ranchoice == 'p':win()
+        else:tie()
     elif choice == 'e':exit()
     else:print("invalid")
     time.sleep(1)
